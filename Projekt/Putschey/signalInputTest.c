9f87f3a133e4b78de0cb5ed69e41cca2732b5504
Darkedge113 <nero113@gmx.at>
2018-12-06 17:17:08 +0000

    Auslesen funktionrte! Nachholen!

--- Projekt/Putschey/signalInputTest.c
+++ Projekt/Putschey/signalInputTest.c
@@ -16,6 +16,8 @@ Signal end = Output of Sensor is logical high
 #define BAUDRATE 115200
 #define MAXSIZE 1000
 #define SENSORINPUT PB3 //externe Interrupt for timer 1 triggers on PB1
+#define TIMER0_OFFSET 256-52 //reload value of 204 with 0,5µs steps = 26µs = T -> f = 38kHz
+#define IRLED PD4 // Pin for IR led
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -23,9 +25,9 @@ Signal end = Output of Sensor is logical high
 #include <avr/sleep.h>
 
 volatile uint16_t globalSensorInputArray[MAXSIZE];
-volatile static uint8_t arrayPosition = 0; //variable value remains after exiting function
 volatile uint8_t arrayFull = 0;
 volatile uint8_t overflowFlag = 0;
+volatile uint8_t signalmode = 1;
 
 void uart_init( uint32_t baud) {
 
@@ -51,16 +53,31 @@ void uart_sendstr( char * str ) {
 	}
 }
 
+void init_timer0() {
+	TCCR0B |= (1 << CS10); // Enable Timer 8 - 8 prescale
+	TIMSK0 |= (1 << TOIE0);
+	TCNT0 = TIMER0_OFFSET; //reload value of 52 with 0,5µs steps = 26µs = T -> f = 38kHz
+}
+void clear_timer0(){
+	TCCR0B &= ~(1 << CS10); // Enable Timer 8 - 8 prescale
+	TIMSK0 &= ~(1 << TOIE0);
+}
 void init_timer1() {
-	//TCCR1B &= ~(1 << COM1B1) & ~(1 << COM1B0);
 	TCCR1B |= (1 << CS12); // select timer1 prescaler of 1 (62.5 ns resolution)
-	TIMSK1 |= (1 << TOIE1); // TIMSK0 |= (1<<TOIE0);
+	TIMSK1 |= (1 << TOIE1);
 	//TCNT1 = 0; // to init with reload value
 }
 void reset_timer1() {
 	TCNT1 = 0; // to init with reload value
 }
+void init_timer2() {
+	TCCR2B |= (1 << CS00); // Enable Timer 0 - No prescale
+	TIMSK2 |= (1 << TOIE2);
+	//TCNT2 = TIMER0_OFFSET; //reload value of 52 with 0,5µs steps = 26µs = T -> f = 38kHz
+}
 void init_register() {
+	DDRD |= (1 << IRLED);
+	//PORTD |= (1 << IRLED);
 	DDRB  &= ~(1 << SENSORINPUT); //set PB0 as input
 	PORTB |= (1 << SENSORINPUT);  // set PB0 pullup
 }
@@ -69,8 +86,6 @@ void init_externalInterrupt() {
 	EIMSK |= (1 << INT1);     // Turns on INT0
 	EICRA |= (1 << ISC10);    // set INT0 to trigger on ANY edge
 }
-
-
 void intToString(uint16_t val, char * target) { /* 2^16-1 =  5 digits + 1 for string terminator */
 	int16_t index;
 	for(index = 4; index >= 0; index--) {
@@ -80,8 +95,14 @@ void intToString(uint16_t val, char * target) { /* 2^16-1 =  5 digits + 1 for st
 	target[5] = '\0';
 }
 
+ISR(TIMER2_OVF_vect) {
+
+}
 ISR (INT1_vect) { //is called for ANY edge
 	static uint8_t firstFlank = 1;
+	static uint8_t arrayPosition = 0; //variable value remains after exiting function
+
+	uart_sendstr("TEST");
 
 	if (firstFlank) {
 		reset_timer1();
@@ -100,9 +121,47 @@ ISR (INT1_vect) { //is called for ANY edge
 		}
 	}
 }
+ISR(TIMER0_OVF_vect) {
+	PORTD ^= (1 << IRLED);
+	TCNT0 = TIMER0_OFFSET; //wird mit 38kHz gerufen
+}
 
 ISR(TIMER1_OVF_vect){
 	overflowFlag = 1;
+	if (signalmode) {
+		init_timer0();
+	}
+	else {
+		clear_timer0();
+	}
+}
+
+void outputCommand() {
+	uart_sendstr("Ausgabe des Arrays\n\r");
+	uint16_t index;
+	char convertedString[6];
+	for (index = 0; index < MAXSIZE; index++){
+		intToString(globalSensorInputArray[index], convertedString);
+		uart_sendstr(convertedString);
+		uart_sendstr("\r\n");
+
+		if(globalSensorInputArray[index] == 0) {
+			_delay_ms(300);
+			uart_sendstr("Ende des Arrays\n\r");
+			return;
+		}
+	}
+}
+
+void sendCommand(uint16_t * sendArray) {
+	uint16_t index;
+	while (overflowFlag) {
+		TCNT1 = 65535 - globalSensorInputArray[index];
+		signalmode = !signalmode;
+		index++;
+		while (!overflowFlag) {}
+		overflowFlag = 0;
+	}
 }
 
 int main() {
@@ -112,7 +171,21 @@ int main() {
 	init_timer1();
 	sei();
 
-	while(1) {
+	_delay_ms(500);
+	sendCommand(globalSensorInputArray);
 
+	uart_sendstr("Ausgabe des Arrays\n\r");
+	uint16_t index;
+	char convertedString[6];
+	for (index = 0; index < MAXSIZE; index++){
+		intToString(globalSensorInputArray[index], convertedString);
+		uart_sendstr(convertedString);
+		uart_sendstr("\r\n");
+
+		if(globalSensorInputArray[index] == 0) {
+			_delay_ms(300);
+			uart_sendstr("Ende des Arrays\n\r");
+			return;
+		}
 	}
 }
