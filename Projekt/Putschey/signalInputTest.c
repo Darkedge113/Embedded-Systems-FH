42887d8c5f8a04aacfe1840afed2252a8a358374
Darkedge113 <nero113@gmx.at>
2018-12-10 12:13:32 +0000

    Commented and optimized code

--- Projekt/Putschey/signalInputTest.c
+++ Projekt/Putschey/signalInputTest.c
@@ -1,52 +1,41 @@
 /*
-Read in the InfraRed Signal via Sensor
-
-Sensor standy-by output = logical high
-Sensor receives -> output = logical low
-
-First falling edge -> Timer 1 starts
-
-Rising edge -> Save time in array and reset Timer 1
-Falling edge -> Save time in array and reset Timer 1
-Repeat until Array full, or Signal ends
-Signal end = Output of Sensor is logical high
+10.12.2018
+Author: Eres Robert
+Technikum-ID: 1710768026
 */
 
 #define F_CPU 16000000
 #define BAUDRATE 115200
-#define MAXSIZE 1000
-#define SENSORINPUT PD3 //externe Interrupt for timer 1 triggers on PB1
-#define TIMER0_OFFSET 256-52 //reload value of 204 with 0,5µs steps = 26µs = T -> f = 38kHz
+#define MAXSIZE 1000 //Maximal size for the incoming IR signal
+#define SENSORINPUT PD3 //External Interrupt for Timer 1 only trigger on pin PD3
+#define TIMER0_OFFSET 256-52 //Reload value of 204 with 0,5µs steps = 26µs = T -> f = 38kHz
 #define IRLED PB0 // Pin for IR led
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <avr/sleep.h>
 
-volatile uint16_t globalSensorInputArray[MAXSIZE];
-volatile uint8_t arrayFull = 0;
-volatile uint8_t overflowFlag = 0;
-volatile uint8_t signalmode = 1;
+volatile uint16_t globalSensorInputArray[MAXSIZE]; //Global array for the incoming signal
+volatile uint8_t arrayFull = 0; //Flag shows that the array is full
+volatile uint8_t overflowFlag = 0; //Timer Overflow Flag
+volatile uint8_t signalmode = 1; //To indicate the current height of the signal (low/high)
 
 void uart_init( uint32_t baud) {
 
-	uint16_t BRR =  (F_CPU / (8 * baud)) -1;   // kann größer als 255 sein !
+	uint16_t BRR =  (F_CPU / (8 * baud)) -1;   // 16 bit necessary since it can exceed 255.
 
-	UBRR0H = (BRR >> 8) ;     // get high byte and store it in UBRR0H
-	UBRR0L = (BRR & 0xff);    // low byte
+	UBRR0H = (BRR >> 8) ;     // Get high byte and store it in UBRR0H
+	UBRR0L = (BRR & 0xff);    // Same of the low byte
 
-	UCSR0B |= (1<<TXEN0) + (1<<RXEN0);  // enable UART receiver and transmitter
+	UCSR0B |= (1<<TXEN0) + (1<<RXEN0);  // Enable UART receiver and transmitter
 
-	UCSR0A |= (1<< U2X0);     // activate double speed for UART timing
+	UCSR0A |= (1<< U2X0);     // Activate double speed for UART timing
 }
 void uart_transmit (uint8_t output) {
-
 	while (!(UCSR0A & (1<<UDRE0))) ;
 	UDR0 = output;
 }
 void uart_sendstr( char * str ) {
-
 	while ( * str ) {
 		uart_transmit (* str);
 		str++;
@@ -54,35 +43,33 @@ void uart_sendstr( char * str ) {
 }
 
 void init_timer0() {
-	TCCR0B |= (1 << CS01); // Enable Timer 8 - 8 prescale
-	TIMSK0 |= (1 << TOIE0);
-	TCNT0 = TIMER0_OFFSET; //reload value of 52 with 0,5µs steps = 26µs = T -> f = 38kHz
+	TCCR0B |= (1 << CS01); // Enable Timer 0 - 8 prescale
+	TIMSK0 |= (1 << TOIE0); //Enbable interrupts
+	TCNT0 = TIMER0_OFFSET; //Reload value of 52 with 0,5µs steps = 26µs = T -> f = 38kHz
 }
 void clear_timer0(){
-	TCCR0B &= ~(1 << CS01); // Disable Timer 8 - 8 prescale
-	TIMSK0 &= ~(1 << TOIE0);
+	TCCR0B &= ~(1 << CS01); // Disable Timer0
+	TIMSK0 &= ~(1 << TOIE0); // Disable Interrupts
 	TCNT0 = TIMER0_OFFSET;
 }
 void init_timer1() {
-	TCCR1B |= (1 << CS12); // select timer1 prescaler of 1 (62.5 ns resolution)
+	TCCR1B |= (1 << CS12); // Select timer1 prescaler of 1 (62.5 ns resolution)
 	TIMSK1 |= (1 << TOIE1);
-	//TCNT1 = 0; // to init with reload value
 }
 void reset_timer1() {
-	TCNT1 = 0; // to init with reload value
+	TCNT1 = 0; // To init with reload value of 0
 }
 void init_register() {
-	DDRB |= (1 << IRLED); //Set PB0 as output
-	//PORTB |= (1 << IRLED);
-	DDRD  &= ~(1 << SENSORINPUT); //set PD3 as input
-	PORTD |= (1 << SENSORINPUT);  // set PD3 pullup
+	DDRB |= (1 << IRLED); // Set PB0 as output
+	DDRD  &= ~(1 << SENSORINPUT); // Set PD3 as input
+	PORTD |= (1 << SENSORINPUT);  // Set PD3 pullup
 }
 void init_externalInterrupt() {
 	//EICRA = external interrupt control register
 	EIMSK |= (1 << INT1);     // Turns on INT0
 	EICRA |= (1 << ISC10);    // set INT0 to trigger on ANY edge
 }
-void intToString(uint16_t val, char * target) { /* 2^16-1 =  5 digits + 1 for string terminator */
+void intToString(uint16_t val, char * target) { // 2^16-1 =  5 digits + 1 for string terminator
 	int16_t index;
 	for(index = 4; index >= 0; index--) {
 		target [index] = val % 10 + '0';
@@ -91,13 +78,11 @@ void intToString(uint16_t val, char * target) { /* 2^16-1 =  5 digits + 1 for st
 	target[5] = '\0';
 }
 
-ISR (INT1_vect) { //is called for ANY edge
+ISR (INT1_vect) { // Is called for ANY edge
 	static uint8_t firstFlank = 1;
-	static uint8_t arrayPosition = 0; //variable value remains after exiting function
+	static uint8_t arrayPosition = 0; // Variable value remains after exiting function
 
-
-
-	if (firstFlank) {
+	if (firstFlank) { // To ignore the time before the first flank
 		reset_timer1();
 		firstFlank = 0;
 		return;
@@ -106,9 +91,9 @@ ISR (INT1_vect) { //is called for ANY edge
 		if(arrayPosition >= MAXSIZE-1){
 			arrayFull = 1;
 			return;
-		 }
+		}
 		else {
-			globalSensorInputArray[arrayPosition] = TCNT1;
+			globalSensorInputArray[arrayPosition] = TCNT1; // Saves current timer value in global array
 			reset_timer1();
 			arrayPosition++;
 		}
@@ -116,26 +101,24 @@ ISR (INT1_vect) { //is called for ANY edge
 }
 ISR(TIMER0_OVF_vect) {
 
-	PORTB |= (1 << IRLED); //LED leuchtet NICHT!
+	//PORTB |= (1 << IRLED); //LED leuchtet NICHT!
 
-	PORTB ^= (1 << IRLED);
-	TCNT0 = TIMER0_OFFSET; //wird mit 38kHz gerufen
+	PORTB ^= (1 << IRLED); // Toggles LED
+	TCNT0 = TIMER0_OFFSET; // ISR is called with 38kHz -> Toggles LED with 38kHz
 }
 
-ISR(TIMER1_OVF_vect){
-
-	PORTB |= (1 << IRLED);  //LED leuchtet NICHT!
-
+ISR(TIMER1_OVF_vect){ // ISR called when Timer1 overflows
+	//PORTB |= (1 << IRLED);  //LED leuchtet NICHT!
 	overflowFlag = 1;
 	if (signalmode) {
-		init_timer0();
+		init_timer0(); // Starts timer 0 -> Toggles LED with 38kHz, is 1 in RC5
 	}
 	else {
-		clear_timer0();
+		clear_timer0(); // To stop the toggling -> Is 0 in RC5
 	}
 }
 
-void outputCommand() {
+void outputCommand() { // Short function to output the contente of the global array
 	uart_sendstr("Ausgabe des Arrays\n\r");
 	uint16_t index;
 	char convertedString[6];
@@ -151,7 +134,7 @@ void outputCommand() {
 		}
 	}
 }
-void sendCommand(uint16_t * sendArray) {
+void sendCommand(uint16_t * sendArray) { //Sends the command via IR LED
 	uint16_t index;
 	while (overflowFlag) {
 		TCNT1 = 65535 - globalSensorInputArray[index];
@@ -170,7 +153,6 @@ int main() {
 	sei();
 
 	_delay_ms(500);
-
 
 	sendCommand(globalSensorInputArray);
 	outputCommand();
